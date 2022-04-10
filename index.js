3151813f0529da41b7a91cdaea94e19834d8c8ce
Tomas Hubelbauer <tomas@hubelbauer.net>
2022-04-10 19:33:00 +0200

    Remove the void async wrapper and use TLA
    Top level awaitis supported in recent Node versions.

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
 import getSecret from './getSecret.js';
 import setSecret from './setSecret.js';
 
-void async function () {
-  const date = new Date().toISOString();
+const date = new Date().toISOString();
 
-  console.log('Setting password');
-  await setSecret(date, 'paper', Math.random().toString());
-  console.log('Password set');
+console.log('Setting password');
+await setSecret(date, 'paper', Math.random().toString());
+console.log('Password set');
 
-  console.log('Getting password');
-  console.log(await getSecret(date, 'paper'));
-  console.log('Password got');
-}()
+console.log('Getting password');
+console.log(await getSecret(date, 'paper'));
+console.log('Password got');
